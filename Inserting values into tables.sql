585c56b334d6dfda7abf02f8f658760d77247973
Tumade Danso <71105406+Tdanso@users.noreply.github.com>
2022-01-26 18:31:03 -0500

    Add files via upload

---
+++ Inserting values into tables.sql
@@ -0,0 +1,25 @@
+-- insertingt the values into the videos tables 
+
+INSERT INTO videos VALUES (1, 'Deep Learning Basics: Introduction and Overview',
+						   68, 'https://www.youtube.com/watch?v=O5xeyoRL95U');
+
+INSERT INTO videos VALUES (2, 'Why Some People Succeed and Some Dont',
+						   76, 'https://www.youtube.com/watch?v=EcMKLwVlpJk');
+						   
+INSERT INTO videos VALUES (3, 'Principles For Success by Ray Dalio',
+						   28, 'https://www.youtube.com/watch?v=B9XGUpQZY38');
+
+
+-- insertingt the values into the reviews tables 
+
+INSERT INTO reviews VALUES (1, 'John', 1, 'Too boring',1);
+INSERT INTO reviews VALUES (2, 'Jill', 4, 'Informative',1);
+
+INSERT INTO reviews VALUES (3, 'John', 4, 'Motivates me',2);
+INSERT INTO reviews VALUES (4, 'Jill', 3, 'Subjective',2);
+
+INSERT INTO reviews VALUES (5, 'John', 5, 'Short and to the point',3);
+INSERT INTO reviews VALUES (6, 'Jill', 2, 'No real substance',3);
+
+
+
